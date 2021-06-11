0a5e6ef2b86aa2aa5e3608c98ff8576b0cbc1896
Freyert <Freyert@users.noreply.github.com>
2021-06-11 07:48:39 -0400

    save point

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 import express from 'express'
 import { ApolloServer, gql } from 'apollo-server-express'
 import { readFile } from 'fs/promises'
+import firebase from 'firebase-admin'
 
 async function startApollo() {
     const gqlRaw = await readFile('./schemas/todo.graphql')
     const typeDefs = gql`${gqlRaw.toString()}`
 
+
+    firebase.initializeApp()
+
+    const db = firebase.firestore()
+
+    const collRef = db.collection('todos')
+
+    const docRef = collRef.doc("1")
+
+    docRef.set({
+        title: "finish talk",
+        details: "it's due FRIDAY"
+    })
+
     const resolvers = {
         Query: {
-            todos: () => [
-                {
-                    title: "win",
-                    details: "it all"
-                },
-                {
-                    title: "catch",
-                    details: "'em all"
-                }
-            ]
+            async todos() {
+                var collect = []
+                await collRef.get().then(snapshot => snapshot.forEach(doc => collect.push(doc.data())))
+                console.log(collect)
+                return collect
+            }
         }
     }
 
@@ -27,8 +38,8 @@ async function startApollo() {
     const app = express()
     server.applyMiddleware({app})
 
-    await new Promise(resolve => app.listen({port: 4000}, resolve))
-    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+    await new Promise(resolve => app.listen({port: 8080}, resolve))
+    console.log(`🚀 Server ready at http://localhost:8080${server.graphqlPath}`);
     return { server, app };
 }
 
